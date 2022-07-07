ced389084f7663f4d19ca905bad2bae7c817a782
Daniel Motz <herr.motz.os@gmail.com>
2022-07-07 14:05:04 +0200

    add copy_loop_asm

--- submission_22_07_09_stock_chandla_motz/src/A1/copy_asm.s
+++ submission_22_07_09_stock_chandla_motz/src/A1/copy_asm.s
@@ -1,3 +1,7 @@
+//
+// copy_asm
+//
+
         .text
         .align 4
         .type   copy_asm, %function
@@ -27,3 +31,35 @@ copy_asm:
 
         ret
         .size   copy_asm, (. - copy_asm)
+
+//
+// copy_asm_loop
+//
+
+        .text
+        .align 4
+        .type   copy_asm_loop, %function
+        .global copy_asm_loop
+
+copy_asm_loop:
+        // x0 i_a
+        // x1 o_b
+        // x2 counter
+        // x3 temporary register
+        mov x2, #8
+
+start_loop:
+        ldr x3, [x0]
+        str x3, [x1]
+
+        add x0, x0, #8
+        add x1, x1, #8
+
+        sub x2, x2, #1
+
+        b start_loop
+
+end_loop:
+
+        ret
+        .size   copy_asm_loop, (. - copy_asm_loop)
